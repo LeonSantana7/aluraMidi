d06fbf414c89794256cc006c573d16fe7505f661
Leon Santana <leon.santana854@gmail.com>
2025-03-18 19:10:04 -0300

    Dia 18/03

--- main.js
+++ main.js
@@ -1,37 +1,17 @@
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-function tocaSomTim() {
-    document.querySelector('#som_tecla_tim').play();
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play();
-}
 
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-}
+listaDeTeclas[1].onclick = tocaSomClap;
 
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-}
 
-function tocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-function tocaSomTic() {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-function tocaSomTom() {
-    document.querySelector('#som_tecla_tom').play();
+//enquanto
+while (condition) {
+    listaDeTeclas[0].onclick = tocaSomPom;
 }
